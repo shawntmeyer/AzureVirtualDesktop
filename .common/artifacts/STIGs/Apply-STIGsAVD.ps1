fc95186a95842122d21e07721d3b7cab9c1b5131
Shawn Meyer <shmeyer@microsoft.com>
2025-02-05 04:37:07 -0500

    stig script update

--- .common/artifacts/STIGs/Apply-STIGsAVD.ps1
+++ .common/artifacts/STIGs/Apply-STIGsAVD.ps1
@@ -556,7 +556,7 @@ Write-Log -Message "Getting List of Applicable GPO folders."
 
 $GPOFolders = Get-ChildItem -Path $Script:TempDir -Directory
 [array]$ApplicableFolders = $GPOFolders | Where-Object {$_.Name -like "DoD*Windows $osVersion*" -or $_.Name -like 'DoD*Edge*' -or $_.Name -like 'DoD*Firewall*' -or $_.Name -like 'DoD*Internet Explorer*' -or $_.Name -like 'DoD*Defender Antivirus*'} 
-If(Get-InstalledApplication -Name 'Office', 'Teams') {
+If(Get-InstalledApplication -Name 'Microsoft 365', 'Office', 'Teams') {
 	$ApplicableFolders += $GPOFolders | Where-Object {$_.Name -match 'M365'} 
 }
 $InstalledAppsToSTIG = (Get-InstalledApplication -Name $AppsToSTIG).SearchString
