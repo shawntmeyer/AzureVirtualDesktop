1b06f05733edc67ccb63baa15c3d9328897e1ae3
Shawn Meyer <shmeyer@microsoft.com>
2025-09-18 16:13:43 -0400

    changed dash to dot

--- deployments/Invoke-ImageBuilds.ps1
+++ deployments/Invoke-ImageBuilds.ps1
@@ -3,7 +3,7 @@ $Date = Get-Date -Format 'yyyyMMddhhmmss'
 $Prefixes = @('CustomPrefix1')
 $DeploymentJobs = @()
 ForEach ($Prefix in $Prefixes) {
-    $ParameterFile = Join-Path -Path $PSScriptRoot -ChildPath "imageManagement\imageBuild\parameters\$Prefix-imagebuild.parameters.json"
+    $ParameterFile = Join-Path -Path $PSScriptRoot -ChildPath "imageManagement\imageBuild\parameters\$Prefix.imagebuild.parameters.json"
     If (Test-Path -Path $ParameterFile) {
         Write-Output "Using parameter file: $ParameterFile"
         $DeploymentJobs += New-AzDeployment -Name "ImageBuild-$Prefix-$Date" -Location 'eastus2' -TemplateFile (Join-Path -Path $PSScriptRoot -ChildPath 'imageManagement\imageBuild\imageBuild.json') -TemplateParameterFile $ParameterFile -AsJob 
