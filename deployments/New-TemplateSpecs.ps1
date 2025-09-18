29a7dbda5461b503a08ff61b3aefaecf83155fbe
Shawn Meyer <shmeyer@microsoft.com>
2025-09-18 06:39:23 -0400

    removed bicep component

--- deployments/New-TemplateSpecs.ps1
+++ deployments/New-TemplateSpecs.ps1
@@ -7,40 +7,10 @@ param (
     [bool]$createNetwork = $true,
     [bool]$createCustomImage = $true,
     [bool]$createHostPool = $true,
-    [bool]$CreateAddOns = $true,
-    [bool]$UpdateBicep = $true
+    [bool]$CreateAddOns = $true
 )
 
 $ErrorActionPreference = 'Stop'
-
-$InstallPath = Join-Path -Path $env:USERPROFILE -ChildPath '.bicep'
-$Bicep = Join-Path -Path $InstallPath -ChildPath 'bicep.exe'
-
-If ($UpdateBicep) {
-    Write-Output 'Updating Bicep CLI'
-    # Create the install folder
-    $installDir = New-Item -ItemType Directory -Path $InstallPath -Force
-    $installDir.Attributes += 'Hidden'
-    # Fetch the latest Bicep CLI binary
-    Write-Output "Downloading Bicep CLI to '$Bicep'."
-    (New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe", $Bicep)
-    # Add bicep to your PATH
-    $currentPath = (Get-Item -path "HKCU:\Environment" ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
-    if (-not $currentPath.Contains("%USERPROFILE%\.bicep")) { setx PATH ($currentPath + ";%USERPROFILE%\.bicep") }
-    if (-not $env:path.Contains($installPath)) { $env:path += ";$installPath" }
-    $Version = (Get-Item $Bicep).VersionInfo.FileVersion
-    Write-Output "Bicep CLI updated to Version: $Version"
-    $BicepInstalled = $true
-} Else {
-    $BicepInstalled = Test-Path -Path $Bicep
-    if ($BicepInstalled) {
-        $Version = (Get-Item $Bicep).VersionInfo.FileVersion
-        Write-Output "Bicep CLI found. Version: $Version"
-    }
-    else {
-        Write-Output 'Bicep CLI not found. Please set $UpdateBicep to $true to download the latest version'
-    }
-}
 
 $Context = Get-AzContext
 If ($null -eq $Context) {
@@ -67,22 +37,12 @@ if ($createResourceGroup) {
 
 if ($createNetwork) {
     Write-Output 'Creating AVD Networking Template Spec'
-    If ($BicepInstalled) {
-        $bicepFile = Join-Path $PSScriptRoot -ChildPath 'networking\networking.bicep'
-        Write-Output "Transpiling Bicep file '$bicepFile' to JSON"
-        Start-Process -FilePath $Bicep -ArgumentList "build $bicepFile" -Wait -NoNewWindow
-    }
     $templateFile = Join-Path $PSScriptRoot -ChildPath 'networking\networking.json'
     $uiFormDefinition = Join-Path $PSScriptRoot -ChildPath 'networking\uiFormDefinition.json'
     New-AzTemplateSpec -ResourceGroupName $ResourceGroupName -Name 'AVD-Networking' -DisplayName 'Azure Virtual Desktop Networking' -Description 'Deploys the networking components to support Azure Virtual Desktop' -TemplateFile $templateFile -UiFormDefinitionFile $uiFormDefinition -Location $Location -Version '1.0.0' -Force
 }
 
 if ($createCustomImage) {
-    If ($BicepInstalled) {
-        $bicepFile = Join-Path -Path $PSScriptRoot -ChildPath 'imageManagement\imageBuild\imageBuild.bicep'
-        Write-Output "Transpiling Bicep file '$bicepFile' to JSON"
-        Start-Process -FilePath $Bicep -ArgumentList "build $bicepFile" -Wait -NoNewWindow
-    }
     Write-Output 'Creating AVD Custom Image Template Spec'
     $templateFile = Join-Path -Path $PSScriptRoot -ChildPath 'imageManagement\imageBuild\imageBuild.json'
     $uiFormDefinition = Join-Path -Path $PSScriptRoot -ChildPath 'imageManagement\imageBuild\uiFormDefinition.json'
@@ -90,11 +50,6 @@ if ($createCustomImage) {
 }
 
 if ($createHostPool) {
-    If ($BicepInstalled) {
-        $bicepFile = Join-Path -Path $PSScriptRoot -ChildPath 'hostpools\hostpool.bicep'
-        Write-Output "Transpiling Bicep file '$bicepFile' to JSON"
-        Start-Process -FilePath $Bicep -ArgumentList "build $bicepFile" -Wait -NoNewWindow
-    }
     $templateFile = Join-Path -Path $PSScriptRoot -ChildPath 'hostpools\hostpool.json'
     $uiFormDefinition = Join-Path -Path $PSScriptRoot -ChildPath 'hostpools\uiFormDefinition.json'
     Write-Output 'Creating AVD Host Pool Template Spec'
@@ -102,11 +57,6 @@ if ($createHostPool) {
 }
 
 if ($CreateAddOns) {
-    If ($BicepInstalled) {
-        $bicepFile = Join-Path -Path $PSScriptRoot -ChildPath 'add-ons\RunCommandsOnVms\main.bicep'
-        Write-Output "Transpiling Bicep file '$bicepFile' to JSON"
-        Start-Process -FilePath $Bicep -ArgumentList "build $bicepFile" -Wait -NoNewWindow
-    }
     $templateFile = Join-Path -Path $PSScriptRoot -ChildPath 'add-ons\RunCommandsOnVms\main.json'
     $uiFormDefinition = Join-Path -Path $PSScriptRoot -ChildPath 'add-ons\RunCommandsOnVms\uiFormDefinition.json'
     Write-Output 'Creating Run Commands on VMs Template Spec'
